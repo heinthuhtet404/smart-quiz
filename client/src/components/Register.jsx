0ce20d009834512aaf11cbb689e1e457acf62da8
heinthuhtet404 <heinthuhtet404@gmail.com>
2025-07-27 23:13:00 +0630

    day2: part 2 added some features

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,15 +2,17 @@ import './Register.css';
 
 function Register() {
   return (
-    <div className="register-container">
-      <h2>Register</h2>
-      <form className='register-form'>
-        <input type="text" placeholder="Name" required />
-        <input type="email" placeholder="Email" required />
-        <input type="password" placeholder="Password" required />
-        <button type="submit">Register</button>
-      </form>
-    </div>
+    <section className='register-page'>
+      <div className="register-container">
+        <h2>Register</h2>
+        <form className='register-form'>
+          <input type="text" placeholder="Name" required />
+          <input type="email" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </section>
   );
 }
 
