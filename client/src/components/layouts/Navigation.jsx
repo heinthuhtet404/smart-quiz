1454ab586b1fc7ed4be11b7268de0a3f9af8e731
heinthuhtet404 <heinthuhtet404@gmail.com>
2025-09-09 19:39:43 +0630

    just left conbine all files

--- client/src/components/layouts/Navigation.jsx
+++ client/src/components/layouts/Navigation.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
-import './Navigation.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navigation.css";
 
 const Navigation = () => {
   return (
-    <nav>
-      {/* <NavLink to="/" end>Home</NavLink> */}
-      <NavLink to="/chat-page">Chat</NavLink>
-      {/* <NavLink to="/video-call">Video Call</NavLink>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/not-found">Not Found</NavLink> */}
+    <nav className="navbar">
+      <div className="logo">
+        <i className="fas fa-leaf"></i>
+        <span>KidsLearn</span>
+      </div>
+      <ul className="nav-list">
+        <li className="nav-item">
+          <NavLink to="/" end className="nav-link">
+            ပင်မစာမျက်နှာ
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/tutorial" className="nav-link">
+            သင်ခန်းစာများ
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/chat-page" className="nav-link">
+            စကားပြောခန်း
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/login" className="nav-link">
+            အကောင့်ဝင်ရန်
+          </NavLink>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
