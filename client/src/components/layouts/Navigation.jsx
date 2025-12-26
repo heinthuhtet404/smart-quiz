77f8a46164b9bbcc38b8710fd752477bb718e1c6
heinthuhtet404 <heinthuhtet404@gmail.com>
2025-12-27 00:26:07 +0630

    same tab and some utilities in progress

--- client/src/components/layouts/Navigation.jsx
+++ client/src/components/layouts/Navigation.jsx
@@ -1,42 +1,49 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./Navigation.css";
+import "./Navigation.css"; // Original CSS 그대로 사용
+
+// Import Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLeaf } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        {/* <i className="fas fa-leaf"></i> */}
-        <span>KidsLearn</span>
-      </div>
-      <ul className="nav-list">
-        <li className="nav-item">
-          <a href="http://localhost:5000/home.html" className="nav-link">
-            ပင်မစာမျက်နှာ
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="http://localhost:5000/home.html#lesson" className="nav-link">
-            သင်ခန်းစာများ
-          </a>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/chat-page" className="nav-link">
-            စကားပြောခန်း
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <a href="http://localhost:5000/profile.html" className="nav-link">
-            ကိုယ်ပိုင်စာမျက်နှာ
-          </a>
-        </li>
-        {/* <li className="nav-item">
-          <NavLink to="/login" className="nav-link">
-            အကောင့်ဝင်ရန်
-          </NavLink>
-        </li> */}
-      </ul>
-    </nav>
+    <header>
+      <nav className="site-nav">
+        <div className="site-nav__logo">
+          <FontAwesomeIcon icon={faLeaf} className="leaf-logo"/>
+          <span>KidsLearn</span>
+        </div>
+        <ul className="site-nav__list">
+          <li className="site-nav__item">
+            <a href="http://localhost:5000/home.html" className="site-nav__link">
+              ပင်မစာမျက်နှာ
+            </a>
+          </li>
+          {/* သင်ခန်းစာများ */}
+          {/* <li className="site-nav__item">
+            <a href="/home.html#lesson" className="site-nav__link">
+              သင်ခန်းစာများ
+            </a>
+          </li> */}
+          <li className="site-nav__item">
+            <NavLink
+              to="/chat-page"
+              className={({ isActive }) =>
+                isActive ? "site-nav__link site-nav__link--active" : "site-nav__link"
+              }
+            >
+              စကားပြောခန်း
+            </NavLink>
+          </li>
+          <li className="site-nav__item">
+            <a href="http://localhost:5000/profile.html" className="site-nav__link">
+              ကိုယ်ပိုင်စာမျက်နှာ
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
